f1ba7904c32fcbb4b2c8758495eaa07c5b225c67
ShotaGhoona <shota.yamashita@ghoona.com>
2025-03-29 08:30:28 +0900

    fix: add phoneNumber prop to PhoneContact in UsageFlowSection

--- src/app/UsageFlowSection.tsx
+++ src/app/UsageFlowSection.tsx
@@ -395,7 +395,8 @@ const UsageFlowSection = () => (
             </div>
           </div>
         </div>
-        <PhoneContact className="mt-8" />
+        <PhoneContact phoneNumber="072-320-9209" className="mt-8" />
+
       </div>
     </div>
 );
